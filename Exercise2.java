cdc4a1072d5a27261bec6a628cc440c6207d0d5b
Cruziken <bencruz@vols.utk.edu>
2017-06-19 16:04:18 -0400

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+public class Exercise2 {
+
+	public static void main(String[] args) {
+				System.out.print("Hello World!");
+				System.out.print(" Enter a word.");
+				Scanner scan = new Scanner(System.in);
+				String s = scan.next();
+				System.out.println("Nicely done!!!");
+				
+				
+				
+						
+				
+
+	}
+
+}
