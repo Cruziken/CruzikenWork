26ea6e25c4c202b96cd3806eefe919e2604f72cc
Cruziken <bencruz@vols.utk.edu>
2017-06-20 15:55:46 -0400

    Update Exercise2.java

--- Exercise2.java
+++ Exercise2.java
@@ -1,13 +1,17 @@
-import java.io.*;
 import java.util.Scanner;
 public class Exercise2 {
 
 	public static void main(String[] args) {
 				System.out.print("Hello World!");
+				//The line directly above prints the phrase "Hello World!"
 				System.out.print(" Enter a word.");
+				//Prompts the user for a String.
 				Scanner scan = new Scanner(System.in);
+				//Creates an instance of Scanner.
 				String s = scan.next();
+				//Takes the user input of a string from a command line.
 				System.out.println("Nicely done!!!");
+				//Gives the user congratulations.
 				
 				
 				
