431aaa5f1004e6ca41370fe9181ca9dc4c158abc
Cruziken <bencruz@vols.utk.edu>
2017-06-20 15:56:38 -0400

    Update Exercise3.java

--- Exercise3.java
+++ Exercise3.java
@@ -3,23 +3,31 @@ import java.util.Scanner;
 public class Exercise3 {
 	public static void main(String[] args){
 		System.out.print("Hello World! ");
+		//The line directly above prints the phrase "Hello World!"
 			Scanner scanner = new Scanner(System.in);
+			//Creates an instance of Scanner named scanner.
 			System.out.print("Enter a file name: ");
-		System.out.flush();
+			//Prompts the user for a file name (absolute path).
 			String filename = scanner.nextLine();
+			//Stores variable filename as the user input requested
 			File file = new File(filename);
+			//Changes the variable named filename into a file instance.
 			try {
+			//Exception is thrown to alert the compiler of what to do if it doesn't find the file.
 			Scanner input = new Scanner(file);
+			//Creates an instance of Scanner named input.
 				while (input.hasNext()) {
 					String num = input.nextLine();
-					System.out.print(num);
+					//Creates a string out of Text found within file.
+					System.out.println(num);
+					//Prints num string from past line
 				}
 			}
 		catch(FileNotFoundException e){
+			//Alerts the compiler of what to do in case of the exception.
 			System.err.format("File does not exist\n");
+			//Prints off if FileNotFoundException is true.
 		}
 	}
 }
 	
-
-}
