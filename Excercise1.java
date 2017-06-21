3687835144fd510fd08ab7b5ac71b8b048ed5711
Cruziken <bencruz@vols.utk.edu>
2017-06-21 09:18:08 -0400

    Update Excercise1.java

--- Excercise1.java
+++ Excercise1.java
@@ -1,7 +1,7 @@
 
 public class Excercise1 {
 	public static void main (String[] args){
-		System.out.println("Hello World!");
 		//The line directly above prints the phrase "Hello World!"
+		System.out.println("Hello World!");
 	}
 }
