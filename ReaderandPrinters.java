6dbcd3b39a8fb7d1d7006b3f29a1af402cc0cf09
Cruziken <bencruz@vols.utk.edu>
2017-06-21 09:21:52 -0400

    Update ReaderandPrinters.java

--- ReaderandPrinters.java
+++ ReaderandPrinters.java
@@ -4,33 +4,34 @@ import java.util.Scanner;
 
 
 public class ReaderandPrinters  {
-	private Scanner scanner;
 	//Creates a Scanner named scanner
+	private Scanner scanner;
 	public void ReadandScan() {
-	 scanner = new Scanner(System.in);
 	//Creates an instance of Scanner named scanner.
-	System.out.print("Enter a file name by listing it's absolute path: ");
+	 scanner = new Scanner(System.in);
 	//Prompts the user for a file name (absolute path).
 	//Either have the file in Cruziken folder or C://Users//fz3//....filename.txt
-	final String filename = scanner.nextLine();
+	System.out.print("Enter a file name by listing it's absolute path: ");
 	//Stores variable filename as the user input requested.
-	File file = new File(filename);
+	final String filename = scanner.nextLine();
 	//Changes the variable named filename into a File instance.
-	try {
-		//Exception is thrown to alert the compiler of what to do if it doesn't find the file.
-	Scanner input = new Scanner(file);
+	File file = new File(filename);
+	
+	//Exception is thrown to alert the compiler of what to do if it doesn't find the file.
+	try {	
 		//Creates an instance of Scanner named input.
-		while (input.hasNext()) {
-			String num = input.nextLine();
+	Scanner input = new Scanner(file);
+				while (input.hasNext()) {
 			//Creates a string out of Text found within file.
-			System.out.print(num);
+			String num = input.nextLine();
 			//Prints num string from past line
+			System.out.print(num);
 		}
 	}
-catch(FileNotFoundException e){
 	//Alerts the compiler of what to do in case of the exception.
-	System.err.format("File does not exist\n");
+catch(FileNotFoundException e){
 	//Prints off if FileNotFoundException is true.
+	System.err.format("File does not exist\n");
 }
-}
+}	
 }
