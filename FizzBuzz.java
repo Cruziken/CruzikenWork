131fcf19a1fccf66a9022d1a7e71adea0a088a33
Cruziken <bencruz@vols.utk.edu>
2017-06-22 16:56:18 -0400

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,10 +5,10 @@ public class FizzBuzz {
 	private void Starters(){
 		//Creates an instance of ReaderINI called exc1
 		// ReaderINI exc1 = new ReaderINI();
-		//Creates an instance of TheEnds called exc2
-		 TheEnds exc2= new TheEnds();
-		//Creates an instance of MusicTest1 called exc3
-		MusicTest1 exc3 = new MusicTest1();
+		//Creates an instance of Logiccalled exc2
+		 Logic exc2= new Logic();
+		//Creates an instance of PropINI called exc3
+		PropINI exc3 = new PropINI();
 		//exc1.iniReader();
 		 exc3.doit();
 		 exc2.lesgo();
