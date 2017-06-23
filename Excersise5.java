57cf461805c760deba63ac68a354f7409c3f8bcf
Cruziken <bencruz@vols.utk.edu>
2017-06-23 14:17:45 -0400

    Update Excersise5.java

--- Excersise5.java
+++ Excersise5.java
@@ -8,22 +8,33 @@ private void Start() {
 	
 	//Creates an instance of Screamers called fun1
 	Screamers fun1 = new Screamers();
-	Cpy fun2 = new Cpy();
-	//Creates an instance of Copiers called fun2
-	Copiers fun3 = new Copiers();
+	//Calls the sCream method on fun1
+	fun1.sCream();
+}
+private void Continue() {
 	
-	fun1.Scream();
-	fun2.Proclai();
-	fun2.ReadandSca();
-	fun3.Copyit();
+	//Creates an instance of Cpy called fun2
+	PromptnPrint fun2 = new Cpy();
+	//Calls the proClain method on fun2
+	fun2.proClain();
+	//Calls the readandScan method on fun2
+	fun2.readandScan();
+	
+}
+private void Finish(){
+	//Creates an instance of Copiers called fun3
+	Copiers fun3 = new Copiers();
+	//Calls the copyIt method on fun3 
+	fun3.copyIt();
 }
 
 public static void main(String[] args){
 	// Creates an instance of Excersise5 named task
-	
-Excersise5 task = new Excersise5();
-//Calls the Start method on task. Starts the app
-task.Start();
+	Excersise5 task = new Excersise5();
+	//Calls the Start method on task. Starts the app
+	task.Start();
+	task.Continue();
+	task.Finish();
 
 }
 }
