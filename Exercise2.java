878c8dc47b7e445fd9965b648745ab4204742842
Cruziken <bencruz@vols.utk.edu>
2017-06-23 14:54:48 -0400

    Update Exercise2.java

--- Exercise2.java
+++ Exercise2.java
@@ -2,19 +2,30 @@
 import java.util.Scanner;
 public class Exercise2 {
 
+	private void tellNprompt(){
+		//The line directly below prints the phrase "Hello World!"
+		System.out.print("Hello World!");
+		//Prompts the user for a String.
+		System.out.print(" Enter a word.");
+	}
+		
+	private void scanNimput(){
+		//Creates an instance of Scanner.
+		Scanner scan = new Scanner(System.in);
+		//Takes the user input of a string from a command line.
+		String s = scan.next();
+		//Gives the user congratulations.
+		System.out.println("Nicely done!!!");
+				
+	}
 	public static void main(String[] args) {
-				//The line directly below prints the phrase "Hello World!"
-				System.out.print("Hello World!");
-				//Prompts the user for a String.
-				System.out.print(" Enter a word.");
-				//Creates an instance of Scanner.
-				Scanner scan = new Scanner(System.in);
-				//Takes the user input of a string from a command line.
-				String s = scan.next();
-				//Gives the user congratulations.
-				System.out.println("Nicely done!!!");
-				
-				
+		//Creates a new instance of Exercise2 called exce2
+		Exercise2 exce2 = new Exercise2();
+		//Calls the tellNprompt method on exce2
+		exce2.tellNprompt();
+		//Calls the scanNimput method on exce2
+		exce2.scanNimput();
+		
 				
 				
 						
@@ -23,3 +34,4 @@ public class Exercise2 {
 	}
 
 }
+
