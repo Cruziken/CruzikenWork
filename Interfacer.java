901aed9049619999072521017a496d0ad7fe4e8a
Cruziken <bencruz@vols.utk.edu>
2017-06-23 12:39:09 -0400

    Add files via upload

---
+++ Interfacer.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public interface Interfacer {
+		 static Scanner scanner = new Scanner(System.in);
+		//Creates a filename variable
+		 String filename = scanner.nextLine();
+	
+
+}
