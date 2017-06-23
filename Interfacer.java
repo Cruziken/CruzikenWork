b0e75ebe8c98822ee9df9d8944a0e2577cd14f2a
Cruziken <bencruz@vols.utk.edu>
2017-06-23 12:40:13 -0400

    Update Interfacer.java

--- Interfacer.java
+++ Interfacer.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public interface Interfacer {
+		//Creates an instance of scanner that represents user input
 		 static Scanner scanner = new Scanner(System.in);
 		//Creates a filename variable
 		 String filename = scanner.nextLine();
