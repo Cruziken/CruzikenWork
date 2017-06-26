67f77e3e3f409828a3be5b4ef414334618e9eacf
Cruziken <bencruz@vols.utk.edu>
2017-06-26 09:46:20 -0400

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,21 +2,25 @@
 
 public class FizzBuzz {
 	
-	private void Starters(){
-		//Creates an instance of ReaderINI called exc3
-		// ReaderINI exc3 = new ReaderINI();
+	private void staRters(){
 		//Creates an instance of LogSend called ex1
 		LogSend ex1 = new LogSend();
-		//Creates an instance of Logic called exc2
-		 Logic exc4= new Logic();
-		//Creates an instance of PropINI called exc2
-		PropINI exc2 = new PropINI();
 		//Calls the logIt method on exc1
 		exc1.logIt();
+	}
+	private void conTinuers(){
+		//Creates an instance of PropINI called exc2
+		PropINI exc2 = new PropINI();
 		//Calls the doit method on exc2
 		 exc2.doit();
+	// private void spliTcont(){
+		//Creates an instance of ReaderINI called exc3
+		// ReaderINI exc3 = new ReaderINI();
 		//Calls the iniReader method on exc3
 		//exc3.iniReader();
+	private void finIshers();
+		//Creates an instance of Logic called exc4
+		 Logic exc4= new Logic();
 		//Calls the lesgo method on exc4
 		 exc4.lesgo();
 
@@ -26,8 +30,14 @@ public class FizzBuzz {
 		 public static void main (String[] args){
 			//Creates an instance of FizzBuzz called hope
 			FizzBuzz hope = new FizzBuzz();
-			//Calls the Starters method on hope
-			hope.Starters();
+			//Calls the staRters method on hope
+			hope.staRters();
+			 //Calls the conTinuers method on hope
+			hope.conTinuers();
+			 //Calls the spliTcont method on hope
+			//hope.spliTcont();
+			 //Calls the finIshers method on hope
+			 hope.finIshers();
 				 
 				 
 		
