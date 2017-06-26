628d5c5fc91e3bafa246d34387a1934dad7852fe
Cruziken <bencruz@vols.utk.edu>
2017-06-26 09:57:57 -0400

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -7,18 +7,15 @@ public class FizzBuzz {
 		LogSend ex1 = new LogSend();
 		//Calls the logIt method on exc1
 		exc1.logIt();
-	}
-	private void conTinuers(){
+	
 		//Creates an instance of PropINI called exc2
 		PropINI exc2 = new PropINI();
 		//Calls the doit method on exc2
 		 exc2.doit();
-	// private void spliTcont(){
 		//Creates an instance of ReaderINI called exc3
 		// ReaderINI exc3 = new ReaderINI();
 		//Calls the iniReader method on exc3
 		//exc3.iniReader();
-	private void finIshers();
 		//Creates an instance of Logic called exc4
 		 Logic exc4= new Logic();
 		//Calls the lesgo method on exc4
@@ -32,13 +29,7 @@ public class FizzBuzz {
 			FizzBuzz hope = new FizzBuzz();
 			//Calls the staRters method on hope
 			hope.staRters();
-			 //Calls the conTinuers method on hope
-			hope.conTinuers();
-			 //Calls the spliTcont method on hope
-			//hope.spliTcont();
-			 //Calls the finIshers method on hope
-			 hope.finIshers();
-				 
+			
 				 
 		
 	}
