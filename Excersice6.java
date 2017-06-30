8dd201d28fb132c2b42c7ab5d484a5b8b8788006
Cruziken <bencruz@vols.utk.edu>
2017-06-30 14:58:33 -0400

    Update Excersice6.java

--- Excersice6.java
+++ Excersice6.java
@@ -1,27 +1,27 @@
 package Fizzbuzz;
 
-public class Excersice6 {
+public class Ex6 {
 	public void start() throws Exception {
 		// Creates an instance of INI called phase1
-		INI phase1 = new INI();
+		INI myINI = new INI();
 		// Calls the readIt method on phase1
 		// This will read the text in the INI file and write to log file
-		phase1.readIt();
+		myINI.readIt();
 		// Creates an instance of Logic called phase2
-		Logic phase2 = new Logic();
+		Logic myLogic = new Logic();
 		// Calls the test method on phase2
 		// Executes fizz buzz solution and prints to console and appends to log
 		// file
-		phase2.test();
+		myLogic.test();
 
 	}
 
 	public static void main(String[] args) throws Exception {
 		// Creates an instance of Excersice6 called catalyst
-		Excersice6 catalyst = new Excersice6();
+		Ex6 myEx6 = new Excersice6();
 		// Calls the start method on catalyst
 		// This Starts the program
-		catalyst.start();
+		myEx6.start();
 	}
 
 }
