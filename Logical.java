02fb1dc066a9cc0cd1bd34ebda7cde8e8585d952
Cruziken <bencruz@vols.utk.edu>
2017-07-06 15:51:35 -0400

    Update Logical.java

--- Logical.java
+++ Logical.java
@@ -1,39 +1,33 @@
 package Fizzbuzz;
 
 
-
 public class Logical {
 	// Creates a static integer called max_int and assigns it a value
-	private int max_int = 100;
-	// Creates a integer called lower_divisor and assigns it a value
-	private int lower_divisor = 3;
-	// Creates a integer called upper_divisor and assigns it a value
-	private int upper_divisor = 5;
-	// Creates a String called lower_divisor_label and assigns it a value and
-	private String lower_divisor_label = "fizz";
-	// Creates a String integer called upper_divisor_label and assigns it a
-	// value
-	private String upper_divisor_label = "buzz";
-	// Creates a static integer called x and assigns it a value
-	private int x = 0;
-
+		private int max_int = 100;
+		// Creates a integer called lower_divisor and assigns it a value
+		private int lower_divisor = 3;
+		// Creates a integer called upper_divisor and assigns it a value
+		private int upper_divisor = 5;
+		// Creates a String called lower_divisor_label and assigns it a value and
+		private String lower_divisor_label = "fizz";
+		// Creates a String integer called upper_divisor_label and assigns it a
+		// value
+		private String upper_divisor_label = "buzz";
+		// Creates a static integer called x and assigns it a value
+		private int x = 0;
+	
 	public void test()  {
-		
+		//INIfile myINIfile = new INIfile();
 		Writer myWriter = new Writer("C://Users//Public//Valerie.log");
+		//myWriter.setProperties(myINIfile.getProperties());
 		// Creates while loop that runs until x is less than or equal to 100
 		while (x < max_int) {
 			// Increments the x int by one for as long as the loop condition
 			// lasts
 			x++;
 			
-			// If x is not divisible by 3 or 5 print the integer value of x
-			if ((x % lower_divisor != 0) && (x % upper_divisor != 0)) {
-				// Prints value of x onto console
-				myWriter.writeFile(String.valueOf(x));
-
-			}
 			// If x is divisible by 3 and is by 5 print "fizzbuzz"
-			else if ((x % lower_divisor == 0) && (x % upper_divisor == 0)) {
+			 if ((x % (lower_divisor * upper_divisor) == 0)) {
 				// Prints off fizzbuzz to console
 				myWriter.writeFile(lower_divisor_label + upper_divisor_label);
 			}
@@ -49,13 +43,19 @@ public class Logical {
 			else if (x % upper_divisor == 0) {
 				// Prints off buzz to console
 				myWriter.writeFile(upper_divisor_label);
-
 			}
+			 //If the other conditions aren't met print the number
+			else {
+					// Prints value of x onto console
+					myWriter.writeFile(String.valueOf(x));
+			}
+			
 			
 		}
 
 		// Closes the writer
 		myWriter.closeIt();
-
+	
 	}
 }
+
