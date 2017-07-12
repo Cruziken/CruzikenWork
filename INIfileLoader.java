deaffdd146466f3a913c3a576c8a352ac8403f34
Cruziken <bencruz@vols.utk.edu>
2017-07-12 11:44:15 -0400

    Update INIfileLoader.java

--- INIfileLoader.java
+++ INIfileLoader.java
@@ -12,20 +12,22 @@ import java.util.Properties;
  *
  */
 public class INIfileLoader {
-
 	// Creates a private Property variable named myProps
 	private Properties myProps;
 
 	/**
-	 * This method reads the INI file, sends its content to methods in other
-	 * classes that need the information, and then calls those methods
+	 * This method reads the INI file and returns an instance of Properties
+	 * named myProps as a result
+	 * 
+	 * @return
 	 */
-	public void readIt() {
+
+	public Properties readIt() {
 		// Creates an instance of Writer to access those methods and pass the
 		// desired file name
-		Writer myWriter = new Writer("C://Users//Public//Valerie.log");
+		// Writer myWriter = new Writer("C://Users//Public//Valerie.log");
 		// Creates an instance of Variables to access those methods
-		SetVariables myVariables = new SetVariables();
+		// Variables myVariables = new Variables();
 		// Creates an instance of Properties named myProps
 		myProps = new Properties();
 
@@ -33,29 +35,15 @@ public class INIfileLoader {
 		try {
 			// Loads the keys and values from the character stream from the file
 			myProps.load(new FileInputStream("C://Users//fz3//workspace//FizzBuzz//TheRun.ini"));
-			// Passes the keys and values of myProps to the setProperties
-			// method.
-			// This methods passes the content to the Writer class.
-			myWriter.setProperties(myProps);
-			// Passes the keys and values of myProps to the setProperti method.
-			// This methods passes the content to the Variables class.
-			myVariables.setProps(myProps);
+			
 		}
 		// Catches the exception in case the file does not exist
 		catch (IOException e) {
 			// TODO Auto-generated catch block; Throws NullPointerException
 			e.printStackTrace();
 		}
+		// Whenever this method is called myProps will be the result
+		return myProps;
 
-		// Calls the method in the Writer class that appends the content of the
-		// INI file to another
-		// file and prints out to console
-		myWriter.transfer();
-		// Calls the method that sets some of the content in the INI file equal
-		// to variables and passes these variable values to the LogicController
-		// Class
-		myVariables.setVars();
-		// Calls the method that closes the BufferedReader
-		myWriter.closeIt();
 	}
 }
