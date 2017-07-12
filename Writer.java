d685c922c8edfd9e0083cace4300fc99b1be5c4f
Cruziken <bencruz@vols.utk.edu>
2017-07-12 11:40:23 -0400

    Update Writer.java

--- Writer.java
+++ Writer.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringWriter;
+//import java.io.StringWriter;
 import java.util.Properties;
 
 /**
@@ -19,6 +19,7 @@ public class Writer {
 	private BufferedWriter buffWriter;
 	// Creates an a Properties variable named props
 	private Properties props;
+	private Variables variables;
 
 	/**
 	 * Method allows the passing of filenames from other classes. Creates a new
@@ -52,7 +53,7 @@ public class Writer {
 	 * 
 	 * @param content
 	 */
-	public void writeFile(String content) {
+	public void log(String content) {
 
 		// In case no content is there to be read...
 		try {
@@ -88,40 +89,61 @@ public class Writer {
 	}
 
 	/**
-	 * This method takes the keys and values of the properties f, converts them
-	 * to strings and has them passed to the writeFile method
+	 * This method takes this specific instance of variables that is passed
+	 * through, gets the values associated with the keys in the file, and passes
+	 * them through the log(String content) method in Writer
 	 */
-	public void transfer() {
-		// Creates an instance of StringWriter that is a character stream that
-		// collects its output in a string buffer
-		StringWriter writer = new StringWriter();
-		// In case there is no content in the property file...
-		try {
-			// Writes the keys and values to the StringWriter
-			this.props.store(writer, "");
-		}
-		// ...Do this
-		catch (IOException e) {
-			// TODO Auto-generated catch block; Throws NullPointerException
-			e.printStackTrace();
-		}
-		// Takes the StringWriter, returns a string buffer, then finally returns
-		// a String named INIcontent
-		String INIcontent = writer.getBuffer().toString();
-		// Passes the content from the INIfile to the writeFile method
-		writeFile(INIcontent);
+	public void log(Variables variables) {
+		// Sets the variables equal to whatever instance of Variables is passed
+		// through
+		this.variables = variables;
+		// Gets the value associated with the key max_int
+		int max_int_key = variables.getMax_Int();
+		// Gets the value associated with the key lower_divisor
+		int lower_divisor_key = variables.getLower_Divisor();
+		// Gets the value associated with the key upper_divisor
+		int upper_divisor_key = variables.getUpper_Divisor();
+		// Gets the value associated with the key lower_divisor_label
+		String lower_divisor_label_key = variables.getLower_Divisor_Label();
+		// Gets the value associated with the key upper_divisor_label
+		String upper_divisor_label_key = variables.getUpper_Divisor_Label();
+		// Prints off "max_int = 100" to another file and the console
+		log("max_int = " + max_int_key);
+		// Prints off "lower_divisor" = 3" to another file and the console
+		log("lower_divisor_key = " + lower_divisor_key);
+		// Prints off "upper_divisor" = 5" to another file and the console
+		log("upper_divisor_key = " + upper_divisor_key);
+		// Prints off "lower_divisor_label" = fizz" to another file and the
+		// console
+		log("lower_divisor_key = " + lower_divisor_label_key);
+		// Prints off "upper_divisor_label" = buzz" to another file and the
+		// console
+		log("upper_divisor_key = " + upper_divisor_label_key);
+		
+		//Close the BufferedReader
+		closeIt();
 	}
-
-	/**
-	 * This method accepts the property keys and values from other methods in
-	 * other classes
+	/*
+	 * Alternative method that allows you in main to pass
+	 * myWriter.log(myINI.readIt()) instead of myVariables. This permits the
+	 * reading of the entire fire without having to explicitly string the
+	 * variable names.
 	 * 
-	 * @param properties
+	 * /** This method takes the keys and values of the properties f, converts
+	 * them to strings and has them passed to the writeFile method
+	 * 
+	 * public void log(Properties properties) { // Creates an instance of
+	 * StringWriter that is a character stream that // collects its output in a
+	 * string buffer this.props = properties; StringWriter writer = new
+	 * StringWriter(); // In case there is no content in the property file...
+	 * try { // Writes the keys and values to the StringWriter
+	 * this.props.store(writer, ""); } // ...Do this catch (IOException e) { //
+	 * TODO Auto-generated catch block; Throws NullPointerException
+	 * e.printStackTrace(); } // Takes the StringWriter, returns a string
+	 * buffer, then finally returns // a String named INIcontent String
+	 * INIcontent = writer.getBuffer().toString(); // Passes the content from
+	 * the INIfile to the writeFile method log(INIcontent); closeIt(); }
 	 */
-	public void setProperties(Properties properties) {
-		{
-			// Sets the properties passed through equal to props
-			this.props = properties;
-		}
-	}
+
 }
+
