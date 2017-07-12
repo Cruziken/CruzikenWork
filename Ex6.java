1757cf8adcb0619ce1ee0c54829a7b8592b4fcf1
Cruziken <bencruz@vols.utk.edu>
2017-07-12 11:31:12 -0400

    Update Ex6.java

--- Ex6.java
+++ Ex6.java
@@ -5,18 +5,39 @@ package Fizzbuzz;
  * This class contains the main method that starts the program. 
  *
  */
-public class EX6 {
+public class GenericFizzBuzz {
+	public class GenericFizzBuzz {
 	/**
-	 * Method calls the step to take to begin the program
+	 * Method begins the program
 	 */
 	public static void start() {
 		// Creates an instance of INI called myINI
 		INIfileLoader myINI = new INIfileLoader();
-		// Calls the readIt method on myINI
-		// This will read the text in the INI file and sends the information to
-		// other methods that copy the content to another file and the console, as well as sending the content to the Variables
-		// class to access some of the keys' values to set equal to variables in order to solve the FizzBuzz problem
-		myINI.readIt();
+		// Creates an instance of Writer called myWriter that takes Valerie.log
+		// as a parameter
+		Writer myWriter = new Writer("C://Users//Public//Valerie.log");
+		// Creates an instance of LogicController called myLogicController
+		LogicController myLogicController = new LogicController();
+		// Creates an instance of Variables called myVariables
+		Variables myVariables = new Variables();
+		// Calls the getVars method in myVariables on the method readIt() called
+		// on myINI
+		// MyINI.readIt gets the keys and values of the properties file and
+		// myVariables.getVars takes the keys' values and assigns them to
+		// variables needed to solve the FizzBuzz problem
+		myVariables.getVars(myINI.readIt());
+		// Calls the log method on this particular instance of Variables
+		// This gets the string values for the variables and sends them along
+		// with their associated variable names to the log(String content)
+		// method within Writer
+		myWriter.log(myVariables);
+		// Calls the setVariables method on myLogicController that takes this
+		// specific instance of Variables and gets the varible values nescessary
+		// in order to solve FizzBuzz
+		myLogicController.setVariables(myVariables);
+		// Runs the program that solves the FizzBuzz problems and passes the
+		// results to the log(String content) method in Writers
+		myLogicController.run();
 
 	}
 
