713634dae4a7e81e1e9070b66c2dd0842a503b46
Cruziken <bencruz@vols.utk.edu>
2017-07-14 13:04:02 -0400

    Update INIfileLoader.java

--- INIfileLoader.java
+++ INIfileLoader.java
@@ -1,45 +1,51 @@
 package Fizzbuzz;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 /**
  * 
- * This class holds the method that reads a properties file and has its contents
- * shipped to other methods that need the content and then calls those methods
- * to complete the program
+ * This class contains the main method that starts the program.
  *
  */
-public class INIfileLoader {
-	// Creates a private Property variable named myProps
-	private Properties myProps;
-
+public class GenericFizzBuzz {
 	/**
-	 * This method reads the INI file and returns an instance of Properties
-	 * named myProps as a result
-	 * 
-	 * @return
+	 * Method begins the program
 	 */
-
-	public Properties readIt() {
-		// Creates an instance of Properties named myProps
-		myProps = new Properties();
-		//Creates an instance of myAbPath called myAbPath
-		AbsolutePath myAbPath = new AbsolutePath();
-		// Attempting to scan from file a to file b (file a may not exist)
-		try {
-			// Loads the keys and values from the character stream from the file
-			myProps.load(new FileInputStream(myAbPath.getAbPath("TheRun.ini")));
-			
-		}
-		// Catches the exception in case the file does not exist
-		catch (IOException e) {
-			// TODO Auto-generated catch block; Throws NullPointerException
-			e.printStackTrace();
-		}
-		// Whenever this method is called myProps will be the result
-		return myProps;
+	public static void start() {
+		// Creates an instance of INI called myINI
+		INIfileLoader myINI = new INIfileLoader();
+		// Creates an instance of Writer called myWriter that takes Valerie.log
+		// as a parameter
+		Writer myWriter = new Writer("Valerie.log");
+		// Creates an instance of LogicController called myLogicController
+		LogicController myLogicController = new LogicController();
+		// Creates an instance of Variables called myVariables
+		Variables myVariables = new Variables();
+		// Calls the getVars method in myVariables on the method readIt() called
+		// on myINI
+		// MyINI.readIt gets the keys and values of the properties file and
+		// myVariables.getVars takes the keys' values and assigns them to
+		// variables needed to solve the FizzBuzz problem
+		myVariables.setVars(myINI.readIt());
+		// Calls the log method on this particular instance of Variables
+		// This gets the string values for the variables and sends them along
+		// with their associated variable names to the log(String content)
+		// method within Writer
+		myWriter.log(myINI.readIt());
+		// Calls the setVariables method on myLogicController that takes this
+		// specific instance of Variables and gets the variable values needed
+		// in order to solve FizzBuzz
+		myLogicController.setVariables(myVariables);
+		// Runs the program that solves the FizzBuzz problems and passes the
+		// results to the log(String content) method in Writers
+		myLogicController.run();
 
 	}
+
+	/**
+	 * Method starts the program
+	 */
+	public static void main(String[] args) {
+		// This Starts the program
+		start();
+	}
+
 }
